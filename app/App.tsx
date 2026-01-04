751c0045a344a316f1cd785cf9befe2bcd6bcd6f
Joyewon <dohea770@naver.com>
2026-01-04 22:51:36 +0900

    최초 코드 입력 (디자인 없이 기능만)

---
+++ app/App.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native";
+import SleepScreen from "./feature_sleep/SleepScreen";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <SleepScreen />
+    </SafeAreaView>
+  );
+}
