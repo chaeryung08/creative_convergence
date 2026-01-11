3f59cd99f7cc25b4815f0d51277a0cb73de8eb77
Joyewon <dohea770@naver.com>
2026-01-12 01:03:29 +0900

    폰트설정

--- my_frontend/main.py
+++ my_frontend/main.py
@@ -1,29 +1,18 @@
-from kivy.app import App
-from kivy.core.text import LabelBase    
-from App_sleep.screens.sleep_mode_screen import SleepModeScreen
-
-class SleepMonitorApp(App):
-    def build(self):
-        return SleepModeScreen()
-
-if __name__ == '__main__':
-    SleepMonitorApp().run()
-    from kivy.app import App
 from kivy.core.text import LabelBase
-from App_sleep.screens.sleep_mode_screen import SleepModeScreen
 
-# 🔥 한글 폰트 등록
+# 🔥 폰트 등록 (무조건 제일 위)
 LabelBase.register(
     name='NanumGothic',
-    fn_regular='C:/Windows/Fonts/malgun.ttf', # 맑은 고딕
-    fn_bold='C:/Windows/Fonts/malgunbd.ttf'  # 맑은 고딕 볼드
+    fn_regular='C:/Windows/Fonts/malgun.ttf',
+    fn_bold='C:/Windows/Fonts/malgunbd.ttf'
 )
+
+from kivy.app import App
+from App_sleep.screens.sleep_mode_screen import SleepModeScreen
 
 class SleepMonitorApp(App):
     def build(self):
         return SleepModeScreen()
-        Factory.Label.font_name = 'NanumGothic'
-        Factory.Button.font_name = 'NanumGothic'
-        return SleepModeScreen()
+
 if __name__ == '__main__':
     SleepMonitorApp().run()
