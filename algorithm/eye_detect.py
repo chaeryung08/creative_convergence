87b97515a962e8fc81527cea60844b92e0c4ba3e
이유진 <iyujin@iyujin-ui-MacBookPro.local>
2026-01-12 00:23:02 +0900

    일단 카메라로 졸음 감지 판단 대충 할 수 있게 + main에도 연결

--- algorithm/eye_detect.py
+++ algorithm/eye_detect.py
@@ -6,15 +6,26 @@ import numpy as np
 mp_face_mesh = mp.solutions.face_mesh
 
 LEFT_EYE = [33, 160, 158, 133, 153, 144]
-RIGHT_EYE = [362, 385, 387, 263, 373, 380]
 
 def calculate_ear(eye):
+    """
+    Eye Aspect Ratio (EAR) 계산
+    """
     A = np.linalg.norm(eye[1] - eye[5])
     B = np.linalg.norm(eye[2] - eye[4])
     C = np.linalg.norm(eye[0] - eye[3])
+
+    if C == 0:
+        return 0
+
     ear = (A + B) / (2.0 * C)
+    return ear
 
 def detect_eye_state(frame):
+    """
+    frame: OpenCV BGR 이미지
+    return: "OPEN", "CLOSED", "NO_FACE"
+    """
     if frame is None:
         return "UNKNOWN"
     
@@ -34,19 +45,13 @@ def detect_eye_state(frame):
         h, w, _ = frame.shape
 
         left_eye = np.array([
-        [int(landmarks[i].x * w), int(landmarks[i].y * h)]
-        for i in LEFT_EYE
+            [int(landmarks[i].x * w), int(landmarks[i].y * h)]
+            for i in LEFT_EYE
         ])
 
-        right_eye = np.array([
-        [int(landmarks[i].x * w), int(landmarks[i].y * h)]
-        for i in RIGHT_EYE
-        ])
-
-        ear = (calculate_ear(left_eye) + calculate_ear(right_eye)) / 2
+        ear = calculate_ear(left_eye)
 
         if ear < 0.20:
             return "CLOSED"
         else:
             return "OPEN"
-  
